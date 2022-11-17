61dc97a022398457d396936dca178157937b3aae
Al Camara <Alhousseinec@gmail.com>
2022-11-17 03:22:20 -0600

    adde extra get commends

--- open.sh
+++ open.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
  
-Link=$(kubectl get svc express-server-service \
-    -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+Link=$(kubectl get svc express-server-service -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
 
 echo " this is the browser: $Link"
 
