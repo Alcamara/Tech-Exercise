0a7a280c5d6a358b7ac3e4def6ce6ed1e18022a4
Al Camara <Alhousseinec@gmail.com>
2022-11-17 04:46:58 -0600

    added while loop to open.sh

--- open.sh
+++ open.sh
@@ -2,6 +2,12 @@
  
 Link=$(kubectl get svc express-server-service -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
 
+while [ "$Link" = "" ]
+do
+echo "waiting"
+Link=$(kubectl get svc express-server-service -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+done
+
 echo " this is the browser: $Link"
 
 
