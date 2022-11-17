5da7e4e7bc036aaf571f424ff2a48632491837bf
Al Camara <Alhousseinec@gmail.com>
2022-11-17 02:05:13 -0600

    added open script step

--- open.sh
+++ open.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-kubectl apply -f ./EKS/TE.deployment.yml  
-kubectl apply -f ./EKS/TE.service.yml 
  
 Link=$(kubectl get svc express-server-service \
     -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+
+echo $Link
 
 xdg-open "http://$Link"
 
